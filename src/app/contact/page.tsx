69fc22f4e5e5cd58d98145faca3b2384e04d3608
Karen Borrero <keba2503@gmail.com>
2024-05-25 22:04:28 +0100

    Contact 💄form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,15 +10,15 @@ export interface PageContactProps {}
 
 const info = [
   {
-    title: "🗺 ADDRESS",
+    title: "🗺 Dirección",
     desc: "Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter",
   },
   {
-    title: "💌 EMAIL",
+    title: "💌 Correo electrónico",
     desc: "nc.example@example.com",
   },
   {
-    title: "☎ PHONE",
+    title: "☎ Teléfono",
     desc: "000-123-456-7890",
   },
 ];
@@ -28,7 +28,7 @@ const PageContact: FC<PageContactProps> = ({}) => {
     <div className={`nc-PageContact overflow-hidden`}>
       <div className="mb-24 lg:mb-32">
         <h2 className="my-16 sm:my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
-          Contact
+          Contacto
         </h2>
         <div className="container max-w-7xl mx-auto">
           <div className="flex-shrink-0 grid grid-cols-1 sm:grid-cols-2 gap-12 ">
@@ -45,7 +45,7 @@ const PageContact: FC<PageContactProps> = ({}) => {
               ))}
               <div>
                 <h3 className="uppercase font-semibold text-sm dark:text-neutral-200 tracking-wider">
-                  🌏 SOCIALS
+                  🌏 Redes sociales
                 </h3>
                 <SocialsList className="mt-2" />
               </div>
@@ -53,7 +53,7 @@ const PageContact: FC<PageContactProps> = ({}) => {
             <div>
               <form className="grid grid-cols-1 gap-6" action="#" method="post">
                 <label className="block">
-                  <Label>Full name</Label>
+                  <Label>Nombre completo</Label>
 
                   <Input
                     placeholder="Example Doe"
@@ -62,7 +62,7 @@ const PageContact: FC<PageContactProps> = ({}) => {
                   />
                 </label>
                 <label className="block">
-                  <Label>Email address</Label>
+                  <Label>Dirección de correo</Label>
 
                   <Input
                     type="email"
@@ -71,22 +71,17 @@ const PageContact: FC<PageContactProps> = ({}) => {
                   />
                 </label>
                 <label className="block">
-                  <Label>Message</Label>
+                  <Label>Mensaje</Label>
 
                   <Textarea className="mt-1" rows={6} />
                 </label>
                 <div>
-                  <ButtonPrimary type="submit">Send Message</ButtonPrimary>
+                  <ButtonPrimary type="submit">Enviar mensaje</ButtonPrimary>
                 </div>
               </form>
             </div>
           </div>
         </div>
-      </div>
-
-      {/* OTHER SECTIONS */}
-      <div className="container">
-        <SectionSubscribe2 className="pb-24 lg:pb-32" />
       </div>
     </div>
   );
