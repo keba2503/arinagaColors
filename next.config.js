92fbc9a9c4a9dead47e4e710f7a000e92caee036
Karen <karen.borrero@grupodinosol.es>
2024-06-15 22:49:07 +0100

    Dashboard ✨🔒️ new auth

--- next.config.js
+++ next.config.js
@@ -35,6 +35,11 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 module.exports = nextConfig;
