ddbaa6ab9f3aa6f5e09ea0d2e6784946905e9693
Karen <karen.borrero@grupodinosol.es>
2024-06-15 13:05:41 +0100

    Home  💄 new widget in home

--- src/app/(server-components)/SectionHero.tsx
+++ src/app/(server-components)/SectionHero.tsx
@@ -6,6 +6,21 @@ const SectionHero: React.FC = () => {
     return (
         <div className="relative w-full h-screen">
             <Carrousel />
+            <div className="absolute top-0 left-0 m-4 z-10">
+                <iframe
+                    src="https://api.wo-cloud.com/content/widget/?geoObjectKey=59243104&language=es&region=ES&timeFormat=HH:mm&windUnit=kmh&systemOfMeasurement=metric&temperatureUnit=celsius"
+                    name="CW2"
+                    scrolling="no"
+                    width="170"
+                    height="100"
+                    frameBorder="0"
+                    style={{
+                        borderRadius: "10px",
+                        backgroundColor: "rgba(255, 255, 255, 0.5)",
+                        border: "1px solid #10658E",
+                    }}
+                ></iframe>
+            </div>
             <div className="absolute inset-x-0 bottom-0 flex justify-center z-10 pb-32">
                 <HeroSearchForm />
             </div>
