ae4da2e9809921893b9cbbd24e7eae2444db51be
Karen <karen.borrero@grupodinosol.es>
2024-06-16 12:12:17 +0100

    Fix 🐛 build

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,6 @@
-import {fetchRevenue} from "@/lib/data";
 import {lusitana} from '@/components/ui/fonts';
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
 
     return <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
