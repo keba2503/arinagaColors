ea3f6c24940a4a685d5e29b5cdfe48806fc5bba3
Karen <karen.borrero@grupodinosol.es>
2024-06-23 16:45:55 +0100

    Admin 🔨 refactor carrousel section hero

--- src/components/admin/Forms/SectionHero.js
+++ src/components/admin/Forms/SectionHero.js
@@ -2,6 +2,9 @@
 
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import dynamic from "next/dynamic";
+
+const RichTextEditor = dynamic(() => import('@/components/RichTextEditor'), {ssr: false});
 
 const SectionHero = ({ scope, data }) => {
     const [title, setTitle] = useState('');
@@ -77,13 +80,12 @@ const SectionHero = ({ scope, data }) => {
                 />
             </div>
             <div className="mb-5">
-                <label htmlFor="subtitle-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subtitulo</label>
-                <input
-                    type="text"
-                    id="subtitle-input"
+                <label htmlFor="description-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Párrafo de bienvenida
+                </label>
+                <RichTextEditor
                     value={subtitle}
-                    onChange={(e) => setSubtitle(e.target.value)}
-                    className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    onChange={setSubtitle}
                 />
             </div>
             <div className="flex items-center justify-between">
