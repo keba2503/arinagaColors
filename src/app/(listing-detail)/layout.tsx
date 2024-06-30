7768b0e6ee7fbd40489284ef11e3326e1e05f5f7
Karen <karen.borrero@grupodinosol.es>
2024-06-30 12:28:28 +0100

    Fix 🐛 build

--- src/app/(listing-detail)/layout.tsx
+++ src/app/(listing-detail)/layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import ListingImageGallery from "@/components/listing-image-gallery/ListingImageGallery";
-import {usePathname, useRouter, useSearchParams} from "next/navigation";
-import React, {ReactNode} from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { ReactNode, Suspense } from "react";
 import MobileFooterSticky from "./(components)/MobileFooterSticky";
-import {imageGallery as listingStayImageGallery} from "./listing-stay-detail/constant";
-import {imageGallery as listingYellowImageGallery} from "./yellow/constant";
-import {imageGallery as listingBlueImageGallery} from "./blue/constant";
-import {imageGallery as listingWhiteImageGallery} from "./white/constant";
-import {Route} from "next";
+import { imageGallery as listingStayImageGallery } from "./listing-stay-detail/constant";
+import { imageGallery as listingYellowImageGallery } from "./yellow/constant";
+import { imageGallery as listingBlueImageGallery } from "./blue/constant";
+import { imageGallery as listingWhiteImageGallery } from "./white/constant";
+import { Route } from "next";
 
-const DetailtLayout = ({children}: { children: ReactNode }) => {
+const DetailLayoutContent = ({ children }: { children: ReactNode }) => {
     const router = useRouter();
     const thisPathname = usePathname();
     const searchParams = useSearchParams();
@@ -49,22 +49,19 @@ const DetailtLayout = ({children}: { children: ReactNode }) => {
 
             <div className="container ListingDetailPage__content">{children}</div>
 
-            {/* OTHER SECTION */}
             <div className="container py-24 lg:py-32">
-                {/*<div className="relative py-16">
-          <BackgroundSection />
-          <SectionSliderNewCategories
-            heading="Explore by types of stays"
-            subHeading="Explore houses based on 10 types of stays"
-            categoryCardType="card5"
-            itemPerRow={5}
-            sliderStyle="style2"
-          />
-        </div>*/}
+                {/* Other sections can be added here */}
             </div>
-            {/* STICKY FOOTER MOBILE */}
-            <MobileFooterSticky/>
+            <MobileFooterSticky />
         </div>
+    );
+};
+
+const DetailtLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <DetailLayoutContent>{children}</DetailLayoutContent>
+        </Suspense>
     );
 };
 
