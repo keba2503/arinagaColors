2d51cef4df98a06e9f71b4ad7b83cf5f2b0a75af
Karen <karen.borrero@grupodinosol.es>
2024-09-10 14:29:30 +0100

    Section Hero

--- src/app/(server-components)/SectionHero.tsx
+++ src/app/(server-components)/SectionHero.tsx
@@ -8,7 +8,7 @@ const SectionHero: React.FC = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.src =
-      'https://www.eltiempo.es/widget/widget_loader/feee89ffddbb1e5a6056b0eaa6cb4ec4';
+      'https://www.eltiempo.es/widget/widget_loader/fd8217a89dc399a61ebf740dea64cf9a';
     script.async = true;
     document.body.appendChild(script);
   }, []);
@@ -20,7 +20,7 @@ const SectionHero: React.FC = () => {
         className="absolute top-0 left-0 m-4 z-10"
         style={{ width: '320px', height: '150px' }}
       >
-        <div id="c_feee89ffddbb1e5a6056b0eaa6cb4ec4" className=""></div>
+        <div id="c_fd8217a89dc399a61ebf740dea64cf9a" className="mini"></div>
       </div>
       <div className="absolute inset-x-0 bottom-0 flex justify-center z-10 pb-32">
         <HeroSearchForm />
